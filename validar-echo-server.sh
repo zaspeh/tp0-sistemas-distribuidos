78dec83c7e1ecbcdd75aa030d2f684e28179f9c1
Zaspeh <mdib@fi.uba.ar>
2026-03-15 07:03:53 +0000

    Add: Script que permite verificar que el servidor reciba y envíe el mismo mensaje

---
+++ validar-echo-server.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+NETWORK=$(docker network ls --format '{{.Name}}' | grep testing_net)
+SERVER="server"
+
+PORT=$(grep -i port server/config.ini | cut -d '=' -f2 | tr -d ' ')
+
+MESSAGE="hola"
+
+RESPONSE=$(docker run --rm --network $NETWORK alpine sh -c "apk add --no-cache netcat-openbsd > /dev/null && echo $MESSAGE | nc $SERVER $PORT")
+
+if [ "$RESPONSE" = "$MESSAGE" ]; then
+    echo "action: test_echo_server | result: success"
+else
+    echo "action: test_echo_server | result: fail"
+fi
