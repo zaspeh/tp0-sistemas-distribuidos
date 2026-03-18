5424b18de6840220cfd0a77d32b550e5efbfb68f
Zaspeh <mdib@fi.uba.ar>
2026-03-18 13:17:38 +0000

    Add: el cliente envía la apuesta

--- client/common/client.go
+++ client/common/client.go
@@ -62,20 +62,21 @@ func (c *Client) createClientSocket() error {
 
 // StartClientLoop Send messages to the client until some time threshold is met
 func (c *Client) StartClientLoop() {
-	// There is an autoincremental msgID to identify every message sent
-	// Messages if the message amount threshold has not been surpassed
 	for msgID := 1; msgID <= c.config.LoopAmount; msgID++ {
-		// Create the connection the server in every loop iteration. Send an
+
 		c.createClientSocket()
 
-		// TODO: Modify the send to avoid short-write
-		fmt.Fprintf(
-			c.conn,
-			"[CLIENT %v] Message N°%v\n",
-			c.config.ID,
-			msgID,
-		)
-		msg, err := bufio.NewReader(c.conn).ReadString('\n')
+		// envío la apuesta :)
+		err := c.SendBet()
+		if err != nil {
+			log.Errorf("action: send_message | result: fail | client_id: %v | error: %v",
+				c.config.ID,
+				err,
+			)
+			return
+		}
+
+		_, err = bufio.NewReader(c.conn).ReadString('\n')
 		c.conn.Close()
 
 		if err != nil {
@@ -86,16 +87,45 @@ func (c *Client) StartClientLoop() {
 			return
 		}
 
-		log.Infof("action: receive_message | result: success | client_id: %v | msg: %v",
-			c.config.ID,
-			msg,
+		bet := c.config.Bet
+
+		log.Infof(
+			"action: apuesta_enviada | result: success | dni: %s | numero: %s",
+			bet.DNI,
+			bet.Numero,
 		)
 
-		// Wait a time between sending one message and the next one
 		time.Sleep(c.config.LoopPeriod)
-
 	}
+
 	log.Infof("action: loop_finished | result: success | client_id: %v", c.config.ID)
+}
+
+// modularizo en enviarApuesta
+func (c *Client) SendBet() error {
+	data := SerializeBet(c.config.Bet)
+
+	totalWritten := 0
+	for totalWritten < len(data) {
+		n, err := c.conn.Write(data[totalWritten:])
+		if err != nil {
+			return err
+		}
+		totalWritten += n
+	}
+
+	return nil
+}
+
+func SerializeBet(b Bet) []byte {
+	msg := fmt.Sprintf("%s|%s|%s|%s|%s\n",
+		b.Nombre,
+		b.Apellido,
+		b.DNI,
+		b.Nacimiento,
+		b.Numero,
+	)
+	return []byte(msg)
 }
 
 func (c *Client) Close() error {
