37aadb8749b1fca4bb82d32b25fafe87711575a7
Zaspeh <mdib@fi.uba.ar>
2026-03-22 04:32:15 +0000

    Add: Logs de debug al finalizar el envío de batches d eun cliente

--- server/common/server.py
+++ server/common/server.py
@@ -86,6 +86,7 @@ class Server:
         return c
 
     def mark_client_done(self, client_sock):
+        logging.info("action: agregar_cliente_listo | result: success | clientes_totales = %d", self.total_clients)
         self.finished_clients.add(client_sock)
 
         if len(self.finished_clients) == self.total_clients:
